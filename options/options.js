4db620b87239dd5b5e356170828ac5f5c247519d
Crowdbotics <team@crowdbotics.com>
2021-10-06 11:45:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "art_museum_app_31151",
+  url: "https://art_museum_app_31151.botics.co",
+  api: "https://art_museum_app_31151.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
